83e3e96903a21952638359735100e52249636072
Alphy2023 <smarthr406@gmail.com>
2025-06-10 20:16:50 +0300

    login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import LoginPageContent from "./_components/login-page-content";
 
 
 export default function LoginPage() {
-
+  const test=""
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <LoginPageContent/>
