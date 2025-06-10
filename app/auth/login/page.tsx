9684918e50a33f28f0146af5c89309dd4b1fb331
Alphy2023 <smarthr406@gmail.com>
2025-06-10 20:19:10 +0300

    fixing deployment bug

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,7 +3,6 @@ import LoginPageContent from "./_components/login-page-content";
 
 
 export default function LoginPage() {
-  const test=""
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <LoginPageContent/>
